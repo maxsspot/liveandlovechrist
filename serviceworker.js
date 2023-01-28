481fc2d6b478384e3cc57af44a932740bb5b91a9
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2023-01-28 12:41:47 -0500

    Create serviceworker.js

---
+++ serviceworker.js
@@ -0,0 +1,16 @@
+let CACHE_NAME = 'v1.0';
+let urlsToCache = [
+    '/',
+    '/index.html',
+];
+
+self.addEventListener('install', function(event) {
+// Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+        })
+    );
+});
